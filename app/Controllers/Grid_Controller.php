cfa81031bdc39eaaa5e8cc536ec2862d9d69ab2d
Robson Henrique <natsutnitts@gmail.com>
2024-05-09 22:45:06 -0300

    Ajuste Grid

--- app/Controllers/Grid_Controller.php
+++ app/Controllers/Grid_Controller.php
@@ -3,21 +3,72 @@
 namespace App\Controllers;
 
 use App\Models\Grid_Model;;
+
 use CodeIgniter\Controller;
 
-class Grid_Controller extends Controller {
+class Grid_Controller extends Controller
+{
 
-    public function index() {
+    public function index()
+    {
         $gridModel = new Grid_Model();
         $data['protocolosrecebidos'] = $gridModel->getAllProtocolos();
-        return view("Grid",$data);
+        return view("Grid", $data);
     }
 
-    public function delete($id)
+    public function atualizarStatus()
     {
-        $protocoloModel = new Grid_Model();
-        $protocoloModel->deleteProtocolo($id);
-        return redirect()->back()->with("status", "O Protocolo foi deletado");
+        $authorName = $this->request->getPost('author_name');
+        $newStatus = $this->request->getPost('new_status');
+        //dd($authorName,$newStatus);
+        $model = new Grid_Model();
+        $result = $model->updateStatus($authorName, $newStatus);
+
+        if ($result) {
+            return $this->response->setJSON(['success' => true]);
+        } else {
+            return $this->response->setJSON(['success' => false]);
+        }
     }
+
+    public function download_pasta($matricula)
+    {
+        $pasta_path = FCPATH . 'assets/' . $matricula;
+        
+        if (!is_dir($pasta_path)) {
+
+            return redirect()->back()->with('error', 'A pasta não existe.');
+        }
+
+        $zip_path = FCPATH . 'assets/' . $matricula . '.zip';
+        $zip = new \ZipArchive();
+        if ($zip->open($zip_path, \ZipArchive::CREATE | \ZipArchive::OVERWRITE) !== TRUE) {
+            return redirect()->back()->with('error', 'Não foi possível criar o arquivo ZIP.');
+        }
+
+        $files = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($pasta_path),
+            \RecursiveIteratorIterator::LEAVES_ONLY
+        );
+        foreach ($files as $name => $file) {
+            if (!$file->isDir()) {
+                $filePath = $file->getRealPath();
+                $relativePath = substr($filePath, strlen($pasta_path) + 1);
+
+                $zip->addFile($filePath, $relativePath);
+            }
+        }
+
+        $zip->close();
+
+        return $this->response->download($zip_path, null)->setFileName($matricula . '.zip');
+    }
+
+    // public function delete($id)
+    // {
+    //     $protocoloModel = new Grid_Model();
+    //     $protocoloModel->deleteProtocolo($id);
+    //     return redirect()->back()->with("status", "O Protocolo foi deletado");
+    // }
 
 }
--- app/Models/Grid_Model.php
+++ app/Models/Grid_Model.php
@@ -5,19 +5,37 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class Grid_model extends Model {
+class Grid_Model extends Model {
 
     protected $table = 'protocolosrecebidos';
     protected $primaryKey = 'protocol_id'; // Se seu id for diferente, ajuste aqui
-    protected $allowedFields = ['protocol_situacaoprofissional', 'protocol_nome', 'protocol_cpf','protocol_matricula'];
+    protected $allowedFields = ['protocol_situacaoprofissional', 'protocol_nome', 'protocol_cpf','protocol_matricula','autor_statusdocumentos'];
 
     public function getAllProtocolos() {
-       return $this->findAll();
+        return $this->select('protocolosrecebidos.*, autores.autor_statusdocumentos')
+                    ->join('autores', 'autores.autor_nome = protocolosrecebidos.protocol_nome')
+                    ->findAll();
+     }
+
+    // public function deleteProtocolo($id)
+    // {
+        
+    //     return $this->delete($id);
+    // }
+
+
+    public function updateStatus($authorName, $newStatus)
+    {
+        // Atualiza a tabela autores onde o autor_nome corresponde ao $authorName
+        $data = [
+            'autor_statusdocumentos' => $newStatus
+        ];
+
+        $this->db->table('autores')
+            ->where('autor_nome', $authorName)
+            ->update($data);
+
+        return $this->db->affectedRows() > 0;
     }
 
-    public function deleteProtocolo($id)
-    {
-        
-        return $this->delete($id);
-    }
 }
